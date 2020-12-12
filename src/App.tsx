72a4fb23b4a1fb3737093f94bbf4672107f36ecb
liuwei <liuwei@talkmoney.cn>
2020-12-12 17:05:24 +0800

    <U> 更新 代码初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Hello from './Components/Hello';
-import MouseTracker from "./Components/MouseTracker";
-import useMousePosition from './hooks/useMousePosition'
-
-interface IThemeProps {
-  [key: string]: {color: string, background: string;}
-}
-const themes: IThemeProps = {
-  'light': {
-    color: '#000',
-    background: '#eee'
-  },
-  'dark': {
-    color: '#fff',
-    background: '#222'
-  }
-}
-export const ThemeContext = React.createContext(themes.light)
 
 function App() {
-  const positions = useMousePosition()
+  const a = '123'
+  if (a == '123') {
+    
+  }
   return (
     <div className="App">
-      <ThemeContext.Provider value={themes.light} >
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>X: {positions.x}, Y:{positions.y}</p>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <MouseTracker />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -41,7 +23,6 @@ function App() {
           Learn React
         </a>
       </header>
-      </ThemeContext.Provider>
     </div>
   );
 }
