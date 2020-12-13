a0255bc84eff27109da9625159c81cc6b2118473
liuwei <liuwei@talkmoney.cn>
2020-12-13 08:36:35 +0800

    <U> 更新 button组件编码

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import Button, { ButtonSize, ButtonType } from './components/Button/button'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button disabled> Hello </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}> Hello </Button>
+        <Button btnType={ButtonType.Link} href="http:www.baidu.com" disabled> 百度Link </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
