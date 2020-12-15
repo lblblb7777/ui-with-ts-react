efd645ba7ff33f23926c49072f9d0b5fb8032220
liuwei <liuwei@talkmoney.cn>
2020-12-15 09:05:47 +0800

    <U> 更新 Menu基础功能完成

--- src/components/Menu/menu.tsx
+++ src/components/Menu/menu.tsx
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import classNames from 'classnames'
 
 type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: number) => void
 export interface MenuProps {
   defaultIndex?: number;
   className?: string;
   mode?: MenuMode;
   style?: React.CSSProperties;
-  onSelect?: (selectedIndex: number) => void
+  onSelect?: SelectCallback;
 }
+
+interface IMenuContext {
+  index: number;
+  onSelect?: SelectCallback;
+}
+
+export const MenuContext = createContext<IMenuContext>({index: 0})
 
 const Menu: React.FC<MenuProps> = (props) => {
   const {
@@ -16,17 +24,29 @@ const Menu: React.FC<MenuProps> = (props) => {
     mode,
     style,
     children,
-    defaultIndex
+    defaultIndex,
+    onSelect
   } = props
+  const [ currentActive, setActive ] = useState(defaultIndex)
   const classes = classNames('menu', className, {
     'menu-vertical': mode === 'vertical'
   })
+  const handleClick = (index: number) => {
+    setActive(index)
+    onSelect && onSelect(index)
+  }
+  const passedContext: IMenuContext = {
+    index: currentActive ? currentActive : 0,
+    onSelect: handleClick
+  }
   return (
     <ul
       className={classes}
       style={style}
     >
-      {children}
+      <MenuContext.Provider value={passedContext}>
+        {children}
+      </MenuContext.Provider>
     </ul>
   )
 }
