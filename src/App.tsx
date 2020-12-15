354fee357dd8adc6a0b7d85160263383d1923c89
liuwei <liuwei@talkmoney.cn>
2020-12-15 08:35:47 +0800

    <U> 更新 Menu组件初始化

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from "react";
 import Button, { ButtonSize, ButtonType } from './components/Button/button'
+import Menu from './components/Menu/menu';
+import MenuItem from './components/Menu/menuItem';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Menu defaultIndex={0}>
+          <MenuItem>
+            cool link
+          </MenuItem>
+          <MenuItem>
+            cool link 2
+          </MenuItem>
+          <MenuItem>
+            cool link 3
+          </MenuItem>
+        </Menu>
+
         <Button className="custom"> Hello </Button>
         <Button> Hello </Button>
         <Button disabled> Disabled Button </Button>
